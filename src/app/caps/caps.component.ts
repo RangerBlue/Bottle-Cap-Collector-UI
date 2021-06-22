8bd8527391e1eda9836376281f1adf3298ac27f7
Kamil Machul <kamil.machul@gmail.com>
2021-06-22 21:58:00 +0200

    Added list of caps

--- src/app/caps/caps.component.ts
+++ src/app/caps/caps.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cap } from '../cap';
+import { CAPS } from '../mock-caps';
 
 @Component({
   selector: 'app-caps',
@@ -11,6 +12,13 @@ export class CapsComponent implements OnInit {
   cap: Cap = {
     id: 1,
     name: "Perla"
+  }
+
+  caps = CAPS;
+
+  selectedCap?: Cap;
+  onSelect(cap: Cap): void {
+    this.selectedCap = cap;
   }
 
   constructor() { }
---
+++ src/app/mock-caps.ts
@@ -0,0 +1,15 @@
+import { Cap } from "./cap";
+
+export const CAPS: Cap[] = [
+    { id: 1, name: 'Perla' },
+    { id: 2, name: 'Lech' },
+    { id: 3, name: 'Tatra' },
+    { id: 4, name: 'Warka' },
+    { id: 5, name: 'Harnas' },
+    { id: 6, name: 'Dziki Wschod' },
+    { id: 7, name: 'Funky Fluid' },
+    { id: 8, name: 'Kasztelan' },
+    { id: 9, name: 'Lubelskie' },
+    { id: 10, name: 'Zywiec' }
+]
+
